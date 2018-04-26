e1ceacf838b34b371e02ec12ec2f7bb5ea30231b
georgezachos <gzorge@gmail.com>
2018-04-26 10:14:04 +0200

    config folder

---
+++ config/cmus/playlist.pl
@@ -0,0 +1,15 @@
+/Users/qfwfq/PhD/LabPrep/Smult.wav
+/Users/qfwfq/Music/Vladislav Delay - Anima (2008)/01 - Anima.flac
+/Users/qfwfq/Music/Vladislav Delay - Anima (2008)/02 - Anima (bonus version).flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/01-Vladislav_Delay-Visaton.flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/02-Vladislav_Delay-Viaton.flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/03-Vladislav_Delay-Viisari.flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/04-Vladislav_Delay-Vihollinen.flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/05-Vladislav_Delay-Viimeinen.flac
+/Users/qfwfq/Music/Vladislav Delay - Anima (2008)/01 - Anima.flac
+/Users/qfwfq/Music/Vladislav Delay - Anima (2008)/02 - Anima (bonus version).flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/01-Vladislav_Delay-Visaton.flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/02-Vladislav_Delay-Viaton.flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/03-Vladislav_Delay-Viisari.flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/04-Vladislav_Delay-Vihollinen.flac
+/Users/qfwfq/Music/Vladislav Delay - Visa (2014)/05-Vladislav_Delay-Viimeinen.flac
